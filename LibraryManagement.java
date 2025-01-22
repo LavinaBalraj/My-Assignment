98b16e226f8e2c1fe0b8f763fc9097e8cca5f99e
LavinaBalraj <lavinabalraj2020@gmail.com>
2025-01-22 17:39:28 +0100

    Add files via upload

---
+++ LibraryManagement.java
@@ -0,0 +1,12 @@
+package week1.day3;
+
+public class LibraryManagement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Library libobject1=new Library();
+		System.out.println(libobject1.addBook("New Book Added"));
+		libobject1.issueBook();
+	}
+
+}
